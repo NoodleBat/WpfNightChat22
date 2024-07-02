59688c80120c165a267271c8a888d0b315431745
admin <admin@DESKTOP-P23BJFG>
2024-07-02 10:52:41 +0300

    Добавьте файлы проекта.

---
+++ WpfLibrary2NNC/IService1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WpfLibrary2NNC
+{
+    // ПРИМЕЧАНИЕ. Можно использовать команду "Переименовать" в меню "Рефакторинг", чтобы изменить имя интерфейса "IService1" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        int Connect();
+
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void SendMsg(string msg, int id);
+    }
+    public interface IServerChatCallBack
+    {
+        [OperationContract(IsOneWay = true)]
+
+        void MsgCallBack(string msg);
+    }
+}
---
+++ WpfLibrary2NNC/ServerUser.cs
@@ -0,0 +1,14 @@
+﻿using WpfLibrary2NNC;
+using System.ServiceModel;
+
+namespace WpfLibrary2NNC
+{
+    internal class ServerUser
+    {
+        public int ID { get; set; }
+
+        public string Name {get; set;}
+
+        public OperationContext operationContext { get; set; }
+    }
+}
